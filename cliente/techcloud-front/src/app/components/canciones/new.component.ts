b83990a4647f1ff164ece5862a6f7e6c65f29cc0
Alonso Rodilla Garcia <60792251+alonsocn10@users.noreply.github.com>
2021-03-24 16:20:08 +0100

    Update new.component.ts

--- cliente/techcloud-front/src/app/components/canciones/new.component.ts
+++ cliente/techcloud-front/src/app/components/canciones/new.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Artistas } from 'src/app/models/artistas';
 import { Canciones } from 'src/app/models/canciones';
+import { ArtistasService } from 'src/app/services/artistas.service';
 import { CancionesService } from 'src/app/services/canciones.service';
 
 @Component({
@@ -12,10 +14,11 @@ import { CancionesService } from 'src/app/services/canciones.service';
 })
 export class NewSongComponent implements OnInit {
 
-  constructor(public cancionesService: CancionesService,
+  constructor(public cancionesService: CancionesService, public artistasService: ArtistasService,
     public router: Router) { }
 
 ngOnInit(): void {
+  this.getArtistas()
 }
 newSong(cancionForm: NgForm){
 this.cancionesService.postCanciones(cancionForm.value)
@@ -32,6 +35,15 @@ if(form){
 form.reset;
 this.cancionesService.selectedCancion = new Canciones();
 }
+}
+getArtistas(){
+  this.artistasService.getArtistas()
+      .subscribe(res =>{
+        this.artistasService.artistas = res as Artistas[];
+        console.log(res)
+      }
+        )
+
 }
 
 }
